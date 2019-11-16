9c9bee3010d9ae8d7f54d2b240ac58f9822db312
Fawaz Ahmed <fawazahmed0@hotmail.com>
2019-11-16 18:13:24 +0530

    Add unsigned driver installer

---
+++ unsigned_driver_installer.bat
@@ -0,0 +1,73 @@
+@echo off
+
+:: Initial message
+echo ====================================================
+echo Unsigned Driver Installer Tool For Windows
+echo By fawazahmed0 @ GitHub
+echo ====================================================
+:: echo. is newline
+echo.
+
+
+:: Source: https://stackoverflow.com/questions/23735282/if-not-exist-command-in-batch-file/23736306
+:: Check for .inf file exists or not
+if not exist *.inf (
+echo Please paste this .bat file in driver folder where .inf file is located
+echo Press any key to exit
+pause > NUL
+exit
+)
+
+:: Source: https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
+:: batch code to request admin previleges
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+:: Source: https://stackoverflow.com/questions/672693/windows-batch-file-starting-directory-when-run-as-admin
+:: Going back to script directory
+cd %~dp0
+
+
+:: Source: https://stackoverflow.com/questions/4619088/windows-batch-file-file-download-from-a-url
+:: Fetching the binaries required for signing the driver
+echo Downloading files required for signing the driver
+PowerShell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/fawazahmed0/windows-unsigned-driver-installer/master/files.zip', 'files.zip')"
+
+
+:: Source: https://stackoverflow.com/questions/37814037/how-to-unzip-a-zip-file-with-powershell-version-2-0
+:: Source: https://www.microsoft.com/en-us/download/details.aspx?id=11800
+:: These files were take from Windows Driver Kit Version 7.1.0
+:: Extracting the .zip file
+PowerShell -Command "& {$shell_app=new-object -com shell.application; $filename = \"files.zip\"; $zip_file = $shell_app.namespace((Get-Location).Path + \"\$filename\"); $destination = $shell_app.namespace((Get-Location).Path); $destination.Copyhere($zip_file.items());}"
+
+:: Source: http://woshub.com/how-to-sign-an-unsigned-driver-for-windows-7-x64/
+:: Signing the Drivers
+echo Signing the drivers
+files\inf2cat.exe /driver:. /os:7_X64 > nul 2>&1
+files\inf2cat.exe /driver:. /os:7_X86 > nul 2>&1
+files\SignTool.exe sign /f files\myDrivers.pfx /p testabc /t http://timestamp.verisign.com/scripts/timstamp.dll /v *.cat > nul 2>&1
+
+:: Adding the Certificates
+files\CertMgr.exe -add files\myDrivers.cer -s -r localMachine ROOT > nul 2>&1
+files\CertMgr.exe -add files\myDrivers.cer -s -r localMachine TRUSTEDPUBLISHER > nul 2>&1
+
+
+:: Source: https://stackoverflow.com/questions/22496847/installing-a-driver-inf-file-from-command-line
+:: Installing Drivers
+echo Installing the drivers
+pnputil -i -a *.inf > nul 2>&1
+
+:: Source: https://social.technet.microsoft.com/Forums/en-US/d109719c-ca97-41e1-a529-0113e23ff5b0/deleting-a-certificate-using-certmgrexe?forum=winserversecurity
+:: Removing the Certificates
+files\CertMgr.exe -del -c -n "Fawaz Ahmed" -s -r localMachine ROOT > nul 2>&1
+files\CertMgr.exe -del -c -n "Fawaz Ahmed" -s -r localMachine TrustedPublisher > nul 2>&1
+
+:: Deleting the temporary items
+echo Deleting the temporary files and folders
+rmdir /Q /S files > nul 2>&1
+del /f files.zip > nul 2>&1
+
+:: Installation done
+echo.
+echo.
+echo Driver Installation complete, press any key to exit
+pause > NUL
